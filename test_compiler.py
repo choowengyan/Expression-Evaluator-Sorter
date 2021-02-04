819f7c849ecc386833f5de483d8c07eb71a321ce
silviana <2b14.9213.silviana@gmail.com>
2021-02-04 16:43:42 +0800

    testing scripts

---
+++ test_compiler.py
@@ -0,0 +1,20 @@
+from compiler.Lexer import Lexer
+from compiler.Parser import Parser
+
+
+# inputs = ['42', '02.2', '', '01', '00', '(', '+']
+# inputs = ['42.0']
+inputs = ['42+1-10', '1 -+- 2 . 9', '1 2 * 2 ** 2']
+# inputs = ['1**2']
+# inputs = ['-2.9']
+# inputs = ['(2+1)*3']
+# inputs = ['2 * 3 + ((1 + 2) - (3 * 4))']
+for i in inputs:
+    # lexer = Lexer(i)
+    # print(lexer.getNextToken())
+    # print(lexer)
+    parser = Parser(i)
+    print(parser.parse())
+    print()
+    # ast = parser.parse(i)
+    # print(ast)
